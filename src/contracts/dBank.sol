f39280b6b8ba84e5753df9e56aa56169599aab00
lionel <lioneldg92@gmail.com>
2021-12-03 12:14:22 +0100

    Init

---
+++ src/contracts/dBank.sol
@@ -0,0 +1,87 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.6.0 <0.8.0;
+
+import "./Token.sol";
+
+contract dBank {
+
+  Token private token;
+
+  mapping(address => uint) public etherBalanceOf;
+  mapping(address => uint) public depositStart;
+  mapping(address => bool) public isDeposited;
+  
+  event Deposit(address indexed user, uint etherAmount, uint timeStart);
+  event Withdraw(address indexed user, uint etherAmount, uint depositTime, uint interest);
+
+  constructor(Token _token) public {
+    token = _token;
+  
+  }
+
+  function deposit() payable public {
+
+    require(isDeposited[msg.sender] == false, 'Error, deposit already active');
+    require(msg.value >= 1e16, 'Error, deposit must be >= 0.01 ETH');
+
+    etherBalanceOf[msg.sender] = etherBalanceOf[msg.sender] + msg.value;
+    depositStart[msg.sender] = depositStart[msg.sender] + block.timestamp;
+
+    isDeposited[msg.sender] = true;
+    
+    emit Deposit(msg.sender, msg.value, block.timestamp);
+  }
+
+  function withdraw() public {
+    require(isDeposited[msg.sender]==true, 'Error, no previous deposit');
+    uint userBalance = etherBalanceOf[msg.sender]; // for event
+
+    uint depositTime = block.timestamp - depositStart[msg.sender];
+
+    // 31668017 - interest(10% APY) per second for min. deposit amount (0.01 ETH):
+    // 1e15(10% of 0.01 ETH) / 31577600 (seconds in 365.25 days)    
+
+    // (etherBalanceOf[msg.sender] / 1e16) - calc. how much higher interest will be (based on depoit time)
+    // for min. deposit (0.01), (etherBalanceOf[msg.sender] / 1e16) = 1 (the same, 31668017/s)
+    // for deposit 0.02, (etherBalanceOf[msg.sender] / 1e16) = 2 (doubled, (2*31668017/s))
+    uint interestPerSecond = 316680170000 * (etherBalanceOf[msg.sender] / 1e16);
+    uint interest = interestPerSecond * depositTime;
+
+    msg.sender.transfer(userBalance);
+    token.mint(msg.sender, interest);
+
+    depositStart[msg.sender] = 0;
+    etherBalanceOf[msg.sender] = 0;
+    isDeposited[msg.sender] = false;
+
+    emit Withdraw(msg.sender, userBalance, depositTime, interest);
+  }
+
+  function borrow() payable public {
+    //check if collateral is >= than 0.01 ETH
+    //check if user doesn't have active loan
+
+    //add msg.value to ether collateral
+
+    //calc tokens amount to mint, 50% of msg.value
+
+    //mint&send tokens to user
+
+    //activate borrower's loan status
+
+    //emit event
+  }
+
+  function payOff() public {
+    //check if loan is active
+    //transfer tokens from user back to the contract
+
+    //calc fee
+
+    //send user's collateral minus fee
+
+    //reset borrower's data
+
+    //emit event
+  }
+}
